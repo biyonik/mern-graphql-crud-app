c8c9d6c88199d48bd7a820df6c0e9935eec90e88
Ahmet Altun <ahmet.altun60@gmail.com>
2022-09-09 10:28:13 +0300

    Proje yapısı oluşturuldu

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portundan dinleniyor...`);
+});
